bb1e66c89ec6630bc4a460ab5b0d04def4568693
79tanka <72666478+79tanka@users.noreply.github.com>
2023-01-09 03:01:15 +0200

    third dist

---
+++ _nuxt/static/1673226044/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fjob-task\u002F_nuxt\u002Fstatic\u002F1673226044",layout:"default",error:a,state:{products:{products:[],product:{}}},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fjob-task\u002F",assetsPath:"\u002Fjob-task\u002F_nuxt\u002F",cdnURL:a}}}}(null));
